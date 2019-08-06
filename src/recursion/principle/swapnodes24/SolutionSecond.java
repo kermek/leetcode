e1d13abec160bae0222631685aede0d47ab768d9
Yermek Kadyrbayev <user@mail.com>
2019-08-06 12:30:11 +0300

    24 improved

---
+++ src/recursion/principle/swapnodes24/SolutionSecond.java
@@ -0,0 +1,32 @@
+package recursion.principle.swapnodes24;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+public class SolutionSecond {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+
+        SolutionSecond solution = new SolutionSecond();
+        solution.swapPairs(head);
+
+    }
+
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode temp = head.next;
+        head.next = swapPairs(head.next.next);
+        temp.next = head;
+        return temp;
+    }
+}
